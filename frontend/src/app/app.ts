e706fd16b85e6b60093b955af53294b1556d89ef
DDarkP <danielfelipenov@gmail.com>
2026-09-18 03:05:09 -0500

    Style/Refactor: mejoras estesteticas y organizacionales a los forms

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,8 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaProductosComponent } from '@features/catalogo/lista-productos/lista-productos';
+import { SidebarComponent } from '@shared/components/sidebar/sidebar';
+import { Layout } from '@shared/components/layout/layout';
+import { ProductoFormComponent } from '@features/catalogo/producto-form/producto-form';
+import { BorradorPedidoComponent } from '@features/mesas/borrador-pedido/borrador-pedido';
+import { DetalleMesaComponent } from '@features/mesas/detalle-mesa/detalle-mesa';
+import { ListaUsuariosComponent } from '@features/usuarios/lista-usuarios/lista-usuarios';
+import { MiPerfilDialogComponent } from '@features/usuarios/mi-perfil-dialog/mi-perfil-dialog';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListaProductosComponent, SidebarComponent, Layout, ProductoFormComponent, BorradorPedidoComponent, DetalleMesaComponent, ListaUsuariosComponent, MiPerfilDialogComponent],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
